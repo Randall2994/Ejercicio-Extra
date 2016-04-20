fad0facdc3c04e229a11e7e4779b3264d140d7b5
Randall2994 <randall291094@hotmail.com>
2016-04-19 23:00:52 -0600

    Agrego el archivo

---
+++ Listas.c
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+typedef struct nodo_t{
+    int x;
+    struct nodo_t *siguiente;
+}nodo;
+
+nodo *root;
+
+
+agregar(nodo*lista){
+    nodo*tmp;
+    nodo*nuevo_nodo;
+    lista=tmp;
+    nuevo_nodo=malloc(sizeof(nodo));
+    
+    
+}
+
+
+
+main(){
+
+}
